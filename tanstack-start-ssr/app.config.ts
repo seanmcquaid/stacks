6bcf3d1ded6986ce19972e4aaaec7f3cdda61bad
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-01-08 18:31:14 +0000

    finish setup

--- tanstack-start-ssr/app.config.ts
+++ tanstack-start-ssr/app.config.ts
@@ -1,9 +1,22 @@
-// app.config.ts
-import { defineConfig } from "@tanstack/start/config";
-import tsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from '@tanstack/start/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   vite: {
-    plugins: [tsConfigPaths()],
+    plugins: [tsConfigPaths(), svgr(), checker({ typescript: true })],
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
+    },
+    build: {
+      rollupOptions: {
+        external: id => id.includes('worker'),
+      },
+    },
   },
 });
