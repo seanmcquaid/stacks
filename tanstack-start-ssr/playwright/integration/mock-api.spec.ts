266fbc66f78ce6541013694f7bfd22e3d21b2b24
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-01-08 23:20:33 +0000

    fixing up test

--- tanstack-start-ssr/playwright/integration/mock-api.spec.ts
+++ tanstack-start-ssr/playwright/integration/mock-api.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from './extensions';
 
 test('Tests against a mocked API in the browser', async ({ page }) => {
-  await page.goto('http://localhost:3000/kitchen-sink');
+  await page.goto('http://localhost:3000');
+  await page.getByRole('link', { name: 'Kitchen Sink' }).click();
   const post = page.getByText('sunt');
   await expect(post).toBeHidden();
 });
