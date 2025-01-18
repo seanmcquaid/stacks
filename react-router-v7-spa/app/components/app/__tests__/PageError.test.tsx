dd1a940a67a765380f7cc1caa2e46a31783829ec
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-01-18 17:50:37 -0500

    app components tests

--- react-router-v7-spa/app/components/app/__tests__/PageError.test.tsx
+++ react-router-v7-spa/app/components/app/__tests__/PageError.test.tsx
@@ -1,7 +1,29 @@
+import { createRoutesStub } from 'react-router';
+import PageError from '../PageError';
+import { render, screen } from '@/utils/testing/reactTestingLibraryUtils';
+
 describe('PageError', () => {
   describe('Title text', () => {
-    it('Displays custom title if provided', () => {});
-    it('Displays default title if custom title is not provided', () => {});
+    it('Displays custom title if provided', () => {
+      const RouteStub = createRoutesStub([
+        { path: '/', Component: () => <PageError titleText="Custom title" /> },
+      ]);
+      render(<RouteStub />);
+      expect(screen.getByText('Custom title')).toBeInTheDocument();
+    });
+    it('Displays default title if custom title is not provided', () => {
+      const RouteStub = createRoutesStub([
+        { path: '/', Component: () => <PageError /> },
+      ]);
+      render(<RouteStub />);
+      expect(screen.getByText('PageError.title')).toBeInTheDocument();
+    });
   });
-  it('Displays error text if provided', () => {});
+  it('Displays error text if provided', () => {
+    const RouteStub = createRoutesStub([
+      { path: '/', Component: () => <PageError errorText="Error message" /> },
+    ]);
+    render(<RouteStub />);
+    expect(screen.getByText('Error message')).toBeInTheDocument();
+  });
 });
