6bddb95d120bf7837be0226634de0a7d29ba8374
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-01-18 21:55:17 -0500

    update it block title

--- react-router-v7-spa/app/services/__tests__/createApiClient.test.ts
+++ react-router-v7-spa/app/services/__tests__/createApiClient.test.ts
@@ -1,5 +1,5 @@
 describe('createApiClient', () => {
-  it('Returns the data if no validation schema is provided', () => {});
+  it('Returns the unvalidated data if no validation schema is provided', () => {});
   describe('Validation schema is provided', () => {
     it('Throws an error if the returned data does not match the validation schema', () => {});
     it('Returns the data if it matches the validation schema', () => {});
