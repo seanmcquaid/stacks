8cf4f1ef0edffd2bd608e0cd0adc176a42562c9b
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-01-18 21:53:32 -0500

    extra case

--- react-router-v7-spa/app/services/__tests__/createApiClient.test.ts
+++ react-router-v7-spa/app/services/__tests__/createApiClient.test.ts
@@ -1,4 +1,5 @@
 describe('createApiClient', () => {
+  it('Returns the data if no validation schema is provided', () => {});
   describe('Validation schema is provided', () => {
     it('Throws an error if the returned data does not match the validation schema', () => {});
     it('Returns the data if it matches the validation schema', () => {});
