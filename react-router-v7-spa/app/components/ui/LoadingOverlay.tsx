3b92134a23c9aa46c8d7b66d035c81fe14566cfb
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-02-01 00:03:56 -0500

    fix linting

--- react-router-v7-spa/app/components/ui/LoadingOverlay.tsx
+++ react-router-v7-spa/app/components/ui/LoadingOverlay.tsx
@@ -9,7 +9,7 @@ const LoadingOverlay = ({ isLoading, ...props }: LoadingOverlayProps) => (
   <div
     className={
       isLoading
-        ? 'fixed bottom-0 left-0 right-0 top-0 z-50 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-white opacity-75'
+        ? 'fixed top-0 right-0 bottom-0 left-0 z-50 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-white opacity-75'
         : 'hidden'
     }
     {...props}
