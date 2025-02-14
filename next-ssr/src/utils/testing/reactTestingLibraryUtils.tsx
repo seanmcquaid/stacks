fbf044a9bec906370a4f17893e48ed2535bcade7
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-02-14 18:09:03 -0500

    more porting

---
+++ next-ssr/src/utils/testing/reactTestingLibraryUtils.tsx
@@ -0,0 +1,29 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import type { RenderResult } from '@testing-library/react';
+import {
+  render as rtlRender,
+  renderHook as rtlRenderHook,
+} from '@testing-library/react';
+import type { ReactElement, PropsWithChildren } from 'react';
+import queryClient from '@/services/queries/queryClient';
+import { Toaster } from '@/components/ui/Toaster';
+
+const Wrapper = ({ children }: PropsWithChildren) => (
+  <QueryClientProvider client={queryClient}>
+    {children}
+    <Toaster />
+  </QueryClientProvider>
+);
+
+const renderHook = <T,>(fn: () => T) => {
+  return rtlRenderHook(fn, { wrapper: Wrapper });
+};
+
+const render = (ui: ReactElement): RenderResult => {
+  return rtlRender(ui, { wrapper: Wrapper });
+};
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export { render, renderHook };
